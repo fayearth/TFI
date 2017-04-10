c23c4177cd660ba97df8854e73527895c85053d1
Stefan Re <kakazekage@gmail.com>
2017-04-10 23:29:05 +0700

    Update and rename tfi-emoji.js to tfiemoji.js

--- js/tfiemoji.js
+++ js/tfiemoji.js
@@ -25,7 +25,7 @@ var twemoji=function(){"use strict";var twemoji={base:"https://twemoji.maxcdn.co
       className: b.className || "emoji",
       callback: function(a, c) {
         switch (a) {case "a9": case "ae": case "2122": return !1 }
-        return "".concat(c.base, a, c.size, c.ext)
+        return "".concat(c.base, a, c.ext)
       },
       onerror: function() {
         g.parentNode && g.parentNode.replaceChild(j.createTextNode(g.alt), g)
